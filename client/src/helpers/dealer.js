e99bf11bfb8dddbf3fb332e70972922381727f05
apedrob <a78305@alunos.uminho.pt>
2020-04-13 23:45:56 +0100

    merge stuff

--- client/src/helpers/dealer.js
+++ client/src/helpers/dealer.js
@@ -2,7 +2,6 @@ import Card from "./card";
 
 export default class Dealer {
   constructor(scene) {
-<<<<<<< HEAD
     this.renderCards = (round, hand) => {
       for (let i = 0; i < round; i++) {
         let playerCard = new Card(scene);
@@ -11,29 +10,6 @@ export default class Dealer {
         let opponentCard = new Card(scene);
         scene.opponentCards.push(
           opponentCard.render(475 + i * 100, 125, "back").disableInteractive()
-=======
-    this.dealCards = (round) => {
-      let playerSprite;
-      let opponentSprite;
-      if (scene.isPlayerA) {
-        playerSprite = "king";
-        opponentSprite = "back";
-      } else {
-        playerSprite = "pirate";
-        opponentSprite = "back";
-      }
-      for (let i = 0; i < round; i++) {
-        let playerCard = new Card(scene);
-        scene.playerCards.push(
-          playerCard.render(475 + i * 100, 650, playerSprite)
-        );
-
-        let opponentCard = new Card(scene);
-        scene.opponentCards.push(
-          opponentCard
-            .render(475 + i * 100, 125, opponentSprite)
-            .disableInteractive()
->>>>>>> 6a47bec17c3fc7b91699fa39654a00db49761325
         );
       }
     };
